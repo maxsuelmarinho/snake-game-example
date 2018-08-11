ef56e6931b0f0da3fb1b9a4bef474462fedce56e
Maxsuel Marihno <maxsuelmarinho@gmail.com>
2018-08-11 02:00:04 -0300

    Define game canvas

---
+++ game/share/app.client.js
@@ -0,0 +1,30 @@
+var Renderer = require('./renderer.js');
+
+var renderer = new Renderer(0, 0, document.getElementById('gameCanvas'));
+
+function resizeGame() {
+  var gameArea = document.getElementById('gameArea');
+  var widthToHeight = 4 / 3;
+  var newWidth = window.innerWidth;
+  var newHeight = window.innerHeight;
+  var newWidthToHeight = newWidth / newHeight;
+
+  if(newWidthToHeight > widthToHeight) {
+    newWidth = newWidth * widthToHeight;
+  } else {
+    newHeight = newWidth / widthToHeight;
+  }
+
+  gameArea.style.width = newWidth + 'px';
+  gameArea.style.height = newHeight + 'px';
+
+  gameArea.style.marginTop = (-newHeight / 2) + 'px';
+  gameArea.style.marginLeft = (-newWidth / 2) + 'px';
+
+  renderer.canvas.width = newWidth;
+  renderer.canvas.height = newHeight;
+}
+
+window.addEventListener('resize', resizeGame, false);
+window.addEventListener('orientationchange', resizeGame, false);
+resizeGame();
