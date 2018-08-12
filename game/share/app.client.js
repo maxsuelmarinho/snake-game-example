2d0cb2c0e9bd69ec97b4583c2fe27f82f5d59a1d
Maxsuel Marihno <maxsuelmarinho@gmail.com>
2018-08-12 01:17:12 -0300

    Rendering the player hero

--- game/share/app.client.js
+++ game/share/app.client.js
@@ -1,5 +1,6 @@
 var Renderer = require('./renderer.js');
 var Game = require('./game.js');
+var Snake = require('./snake.js');
 
 var BLOCK_WIDTH = 16;
 var BLOCK_HEIGHT = 16;
@@ -9,14 +10,33 @@ var renderer =
 var game = new Game(FPS);
 var ctx = renderer.ctx;
 
-game.onUpdate = function() {
+var playerAColor = '#0c0';
+var player = new Snake(
+  parseInt(Math.random() * 999999, 10),
+  playerAColor,
+  parseInt(Math.random() * window.innerWidth / 1.5, 10),
+  parseInt(Math.random() * window.innerHeight / 1.5, 10),
+  BLOCK_WIDTH,
+  BLOCK_HEIGHT
+);
 
+console.log(player);
+
+game.onUpdate = function() {
 };
 
 game.onRender = function() {
   ctx.clearRect(0, 0, renderer.canvas.width, renderer.canvas.height);
-  ctx.fillStyle = '#0000FF';
-  ctx.fillRect(0, 0, renderer.canvas.width, renderer.canvas.height);
+
+  ctx.fillStyle = player.color;
+  player.pieces.forEach(function(piece) {
+    ctx.fillRect(
+      piece.x + player.width,
+      piece.y + player.height,
+      player.width,
+      player.height
+    );
+  });
 };
 
 function resizeGame() {
@@ -45,3 +65,5 @@ function resizeGame() {
 window.addEventListener('resize', resizeGame, false);
 window.addEventListener('orientationchange', resizeGame, false);
 resizeGame();
+
+game.start();
